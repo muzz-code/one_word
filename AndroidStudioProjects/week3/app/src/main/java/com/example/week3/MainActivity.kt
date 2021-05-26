99a091a3866c17c300843b0c4565b6b47d20b3aa
muzz-code <arisepeter247@gmail.com>
2021-05-26 10:47:52 +0100

    final commit

---
+++ AndroidStudioProjects/week3/app/src/main/java/com/example/week3/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.week3
+
+import android.os.Bundle
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        darkMode()
+    }
+    fun doSomething() {
+    }
+
+    fun darkMode() {
+
+        var mode = findViewById<Switch>(R.id.switch1)
+        mode.setOnCheckedChangeListener { _, isChecked ->
+
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+    }
+}
